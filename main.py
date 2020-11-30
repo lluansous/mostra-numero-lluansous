83b6ca7d35a407248bc71dab6649ab560ba3283d
lluansous <luansousa@protonmail.com>
2020-11-30 01:22:51 +0000

    script do numero adcionado com um string.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE O SEU CÓDIGO AQUI.
+    print("Digite um número:")
+    num = input()
+    print(f'O número informado foi {num}')
+
 
 
 if __name__ == '__main__':
